9fc6bb3ab54d402293da5971a8b395be84485538
Kou-kun42 <shinigamithekido@gmail.com>
2020-11-10 17:00:03 -0500

    finished challenge 1

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
     """A homepage with handy links for your convenience."""
     return render_template('home.html')
 
-################################################################################
+###############################################################################
 # COMPLIMENTS ROUTES
-################################################################################
+###############################################################################
+
 
 list_of_compliments = [
     'awesome',
@@ -43,24 +45,30 @@ list_of_compliments = [
     'zoetic'
 ]
 
+
 @app.route('/compliments')
 def compliments():
     """Shows the user a form to get compliments."""
     return render_template('compliments_form.html')
 
+
 @app.route('/compliments_results')
 def compliments_results():
     """Show the user some compliments."""
+    num = int(request.args.get("num_compliments"))
     context = {
-        # TODO: Enter your context variables here.
+        "name": request.args.get("users_name"),
+        "comp": "yes" == request.args.get("wants_compliments"),
+        "num": num,
+        "complis": random.sample(list_of_compliments, k=num)
     }
 
     return render_template('compliments_results.html', **context)
 
 
-################################################################################
+###############################################################################
 # ANIMAL FACTS ROUTE
-################################################################################
+###############################################################################
 
 animal_to_fact = {
     'koala': 'Koala fingerprints are so close to humans\' that they could taint crime scenes.',
@@ -69,6 +77,7 @@ animal_to_fact = {
     'lion': 'Female lions do 90 percent of the hunting.',
     'narwhal': 'Narwhal tusks are really an "inside out" tooth.'
 }
+
 
 @app.route('/animal_facts')
 def animal_facts():
@@ -79,14 +88,15 @@ def animal_facts():
     context = {
         # TODO: Enter your context variables here for:
         # - the list of all animals (get from animal_to_fact)
-        # - the chosen animal fact (may be None if the user hasn't filled out the form yet)
+        # - the chosen animal fact
+        # - (may be None if the user hasn't filled out the form yet)
     }
     return render_template('animal_facts.html', **context)
 
 
-################################################################################
+###############################################################################
 # IMAGE FILTER ROUTE
-################################################################################
+###############################################################################
 
 filter_types_dict = {
     'blur': ImageFilter.BLUR,
@@ -98,16 +108,17 @@ filter_types_dict = {
     'smooth': ImageFilter.SMOOTH
 }
 
+
 def save_image(image, filter_type):
     """Save the image, then return the full file path of the saved image."""
-    # Append the filter type at the beginning (in case the user wants to 
+    # Append the filter type at the beginning (in case the user wants to
     # apply multiple filters to 1 image, there won't be a name conflict)
     new_file_name = f"{filter_type}-{image.filename}"
     image.filename = new_file_name
 
     # Construct full file path
     file_path = os.path.join(app.root_path, 'static/images', file_name)
-    
+
     # Save the image
     image.save(file_path)
 
@@ -121,21 +132,24 @@ def apply_filter(file_path, filter_name):
     i = i.filter(filter_types_dict.get(filter_name))
     i.save(file_path)
 
+
 @app.route('/image_filter', methods=['GET', 'POST'])
 def image_filter():
     """Filter an image uploaded by the user, using the Pillow library."""
     filter_types = filter_types_dict.keys()
 
     if request.method == 'POST':
-        
-        # TODO: Get the user's chosen filter type (whichever one they chose in the form) and save
+
+        # TODO: Get the user's chosen filter type
+        # (whichever one they chose in the form) and save
         # as a variable
         filter_type = ''
-        
+
         # Get the image file submitted by the user
         image = request.files.get('users_image')
 
-        # TODO: call `save_image()` on the image & the user's chosen filter type, save the returned
+        # TODO: call `save_image()` on the image &
+        # the user's chosen filter type, save the returned
         # value as the new file path
 
         # TODO: Call `apply_filter()` on the file path & filter type
@@ -150,26 +164,30 @@ def image_filter():
 
         return render_template('image_filter.html', **context)
 
-    else: # if it's a GET request
+    else:  # if it's a GET request
         context = {
             # TODO: Add context variable here for the full list of filter types
         }
         return render_template('image_filter.html', **context)
 
 
-################################################################################
+###############################################################################
 # GIF SEARCH ROUTE
-################################################################################
+###############################################################################
 
 API_KEY = 'LIVDSRZULELA'
 TENOR_URL = 'https://api.tenor.com/v1/search'
 pp = PrettyPrinter(indent=4)
 
+
 @app.route('/gif_search', methods=['GET', 'POST'])
 def gif_search():
-    """Show a form to search for GIFs and show resulting GIFs from Tenor API."""
+    """
+    Show a form to search for GIFs and show resulting GIFs from Tenor API.
+    """
     if request.method == 'POST':
-        # TODO: Get the search query & number of GIFs requested by the user, store each as a 
+        # TODO: Get the search query & number of GIFs requested by the user,
+        # store each as a
         # variable
 
         response = requests.get(
@@ -193,6 +211,7 @@ def gif_search():
         return render_template('gif_search.html', **context)
     else:
         return render_template('gif_search.html')
+
 
 if __name__ == '__main__':
     app.config['ENV'] = 'development'
